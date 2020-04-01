bdd41ea0afd2e072bb94a8ee54f8f6a365d57b54
Maria shehata <maria.sheahata17@ncf.edu>
2020-03-31 22:52:06 -0400

    started additional Headers in WalmartStoreLocator

--- Delivery_App/Delivery_App/Networking/WalmartStoreLocator.swift
+++ Delivery_App/Delivery_App/Networking/WalmartStoreLocator.swift
@@ -20,6 +20,9 @@ class WalmartStoreLocator {
     }
     
     func startConnection(){
+        // Additional Headers from Walmart Affliate API
+        let sessionConfig = URLSessionConfiguration.default
+        sessionConfig.httpAdditionalHeaders = ["WM_SEC.KEY_VERSION": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3"]
         // URL
         guard let url = URL(string : "https://developer.api.walmart.com/api-proxy/service/affil/product/taxonomy")else{ return}
         let session = URLSession.shared
