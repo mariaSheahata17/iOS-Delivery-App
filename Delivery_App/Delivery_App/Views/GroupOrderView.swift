26df6435b4e36983c3f3ac3d5c14b5fb684b5c90
Maria shehata <maria.sheahata17@ncf.edu>
2020-04-05 23:30:23 -0400

    added GroupOrderView to story board

---
+++ Delivery_App/Delivery_App/Views/GroupOrderView.swift
@@ -0,0 +1,21 @@
+//
+//  GroupOrderView.swift
+//  Delivery_App
+//
+//  Created by Maria Shehata on 4/5/20.
+//  Copyright © 2020 Maria Shehata. All rights reserved.
+//
+
+import UIKit
+
+class GroupOrderView: UIView {
+
+   
+    override func draw(_ rect: CGRect) {
+        self.layer.cornerRadius = 5;
+        self.layer.masksToBounds = true;
+    
+    }
+   
+
+}
